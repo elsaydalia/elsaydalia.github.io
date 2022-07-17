bd4694ec5e0b444f1d876195a0b376a394cb769d
ibrahemesam <iimosa7777@gmail.com>
2022-07-17 05:01:25 +0200

    blabla bla

--- js/peerjs.cfg.js
+++ js/peerjs.cfg.js
@@ -5,4 +5,4 @@ window.PEERJS_SERVER = {
   path: "/",
   // debug: 3, // dbg
 };
-window.SERVER_PEER_ID = "f9caec17-1155-4bc8-bba5-028f59d355df";
+window.SERVER_PEER_ID = "f9caec17-1155-4bc8-bba5-blablabla";
