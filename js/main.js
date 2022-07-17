499264f508b916830791d65feb06c952b542b305
ibrahemesam <iimosa7777@gmail.com>
2022-07-17 04:48:49 +0200

    blabla bla

--- js/main.js
+++ js/main.js
@@ -2,93 +2,109 @@ import "./peerjs.min.js";
 import "./ws_port.js";
 import "./ws_events.js";
 import "./peerjs.cfg.js";
+
+window.readPasteBin = async (pasteId) => {
+  var script = document.createElement("script");
+  document.body.appendChild(script);
+  var OLD_DOCUMENT_WRITE = document.write;
+  var promise_resolve = null;
+  document.write = (str) => {
+    //NEW_DOCUMENT_WRITE
+    document.write = OLD_DOCUMENT_WRITE; // restore old document.write
+    // _ip_🤗_/ip_
+    var ip = str.match(">__(.*)_/_<")[1];
+    promise_resolve(ip);
+  };
+  var promise = new Promise((resolve, reject) => {
+    promise_resolve = resolve;
+  });
+  script.src = `https://pastebin.com/embed_js/${pasteId}`;
+  script.remove();
+  return promise;
+};
+
+window.get_ws_ip = async () => {
+  return await readPasteBin("BDneeqJh");
+};
+
+// (async () => {
+//   var ip = await get_ws_ip();
+//   console.log(ip);
+// })();
+
 (function (root) {
-  window.readPasteBin = async (pasteId) => {
-    var script = document.createElement("script");
-    document.body.appendChild(script);
-    var OLD_DOCUMENT_WRITE = document.write;
-    var promise_resolve = null;
-    document.write = (str) => {
-      //NEW_DOCUMENT_WRITE
-      document.write = OLD_DOCUMENT_WRITE; // restore old document.write
-      // _ip_🤗_/ip_
-      var ip = str.match(">__(.*)_/_<")[1];
-      promise_resolve(ip);
-    };
-    var promise = new Promise((resolve, reject) => {
-      promise_resolve = resolve;
-    });
-    script.src = `https://pastebin.com/embed_js/${pasteId}`;
-    script.remove();
-    return promise;
+  root.ondata = (data) => {
+    console.info("[data]");
+    console.info(data);
+    try {
+      var msg = JSON.parse(data);
+    } catch (error) {
+      console.warn(data);
+      return;
+    }
+    window.ws_events[msg.t](msg);
   };
 
-  window.get_ws_ip = async () => {
-    return await readPasteBin("BDneeqJh");
-  };
+  root.useRtcProxy = () => {
+    var peer = new Peer(undefined, window.PEERJS_SERVER);
+    // console.log(peer.id);
 
-  // (async () => {
-  //   var ip = await get_ws_ip();
-  //   console.log(ip);
-  // })();
-
-  // ===================================
-  // console.log(root);
-  var peer = new Peer(undefined, window.PEERJS_SERVER);
-  // console.log(peer.id);
-
-  peer.on("open", function (id) {
-    // alert(peer.id);
-    var conn = peer.connect(window.SERVER_PEER_ID);
-    // console.info(conn);
-    conn.on("open", function () {
-      // console.log(root); // dbg
-      root.ws = conn;
-    });
-    conn.on("data", function (data) {
-      console.info("[data]");
-      console.info(data);
-      try {
-        var msg = JSON.parse(data);
-      } catch (error) {
-        console.warn(data);
-        return;
-      }
-      window.ws_events[msg.t](msg);
+    peer.on("open", function (id) {
+      // alert(peer.id);
+      var conn = peer.connect(window.SERVER_PEER_ID);
+      // console.info(conn);
+      conn.on("open", function () {
+        // console.log(root); // dbg
+        root.ws = conn;
+      });
+      conn.on("data", root.ondata);
+      // dbg
+      conn.on("error", (e) => {
+        console.log("error");
+        console.error(e);
+      });
+      // dbg
+      conn.on("close", (e) => {
+        console.log("close");
+        console.error(e);
+      });
+      // dbg
+      conn.on("disconnected", () => {
+        console.log("disconnected");
+        console.error(e);
+      });
     });
     // dbg
-    conn.on("error", (e) => {
+    peer.on("error", (e) => {
       console.log("error");
       console.error(e);
     });
     // dbg
-    conn.on("close", (e) => {
+    peer.on("close", (e) => {
       console.log("close");
       console.error(e);
     });
     // dbg
-    conn.on("disconnected", () => {
+    peer.on("disconnected", () => {
       console.log("disconnected");
       console.error(e);
     });
-  });
-  // dbg
-  peer.on("error", (e) => {
-    console.log("error");
-    console.error(e);
-  });
-  // dbg
-  peer.on("close", (e) => {
-    console.log("close");
-    console.error(e);
-  });
-  // dbg
-  peer.on("disconnected", () => {
-    console.log("disconnected");
-    console.error(e);
-  });
-  // console.log(this);
-  // window.peer = peer;
+    // console.log(this);
+    // window.peer = peer;
+  };
+
+  // first, try using WebSocket directly [if success: we are in debugging mode]
+  //       except: fallback to RTC proxy [else: we are in production mode ]
+
+  (function init_ws() {
+    root.ws = new WebSocket(`ws://127.0.0.1:${window.ws_port}/`);
+    root.ws.onmessage = (event) => {
+      root.ondata(event.data);
+    };
+    root.ws.onerror = (err) => {
+      root.useRtcProxy();
+    };
+  })();
 
   window.send = (msg) => {
     root.ws.send(msg);
--- js/peerjs.cfg.js
+++ js/peerjs.cfg.js
@@ -1,9 +1,8 @@
-
 window.PEERJS_SERVER = {
   secure: true,
   host: "family-pharmacy-peerjs.herokuapp.com",
   port: "443",
   path: "/",
+  // debug: 3, // dbg
 };
-window.SERVER_PEER_ID = "server";
-        
+window.SERVER_PEER_ID = "f9caec17-1155-4bc8-bba5-028f59d355df";
